8797885c33373c90f2727691b78286256373f737
Junior Martins <amjr.box@gmail.com>
2021-07-16 17:05:29 -0300

    Finalização da v1.0 do ProgressBar

--- src/components/ProgressBar/progressbar.tsx
+++ src/components/ProgressBar/progressbar.tsx
@@ -1,19 +1,34 @@
+type Colors = {
+  bgColorStart: string;
+  txtColorStart: string;
+  bgColorMid: string;
+  txtColorMid: string;
+  bgColorEnd: string;
+  txtColorEnd: string;
+};
+
 export function ProgressBar({
   progress,
+  status,
   bgColorStart = 'red-500',
   txtColorStart = 'red-200',
-  bgColorEnd = 'green-400',
+  bgColorMid = 'yellow-500',
+  txtColorMid = 'yellow-200',
+  bgColorEnd = 'green-500',
   txtColorEnd = 'green-200'
-}): JSX.Element {
+}: Colors): JSX.Element {
   return (
-    <div className="w-full">
+    <div className={`w-full ${status !== 'show' ? 'hidden' : ''}`}>
       <div className="shadow w-full bg-grey-light mt-2 rounded-full">
         <div
           className={`transition duration-500 ease-in-out h-full text-center font-semibold rounded-full ${
-            progress < 70
+            progress < 33
               ? `bg-${bgColorStart} text-${txtColorStart}`
+              : progress < 66
+              ? `bg-${bgColorMid} text-${txtColorMid}`
               : `bg-${bgColorEnd} text-${txtColorEnd}`
-          }`}
+          }
+        }`}
           style={{ width: `${progress}%` }}
         >
           {progress + '%'}
